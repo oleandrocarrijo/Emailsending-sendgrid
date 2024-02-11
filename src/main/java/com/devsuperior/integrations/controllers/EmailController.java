391e91a23e8f465229bf64abd569f5dac6c7d6e9
Leandro Carrijo <7leandrobatista@gmail.com>
2024-02-10 21:58:02 -0300

    feat: sendEmail created

--- src/main/java/com/devsuperior/integrations/controllers/EmailController.java
+++ src/main/java/com/devsuperior/integrations/controllers/EmailController.java
@@ -1,16 +1,21 @@
 package com.devsuperior.integrations.controllers;
 
 import com.devsuperior.integrations.dto.EmailDTO;
-import org.springframework.http.*;
-import org.springframework.stereotype.Controller;
+import com.devsuperior.integrations.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailController {
 
+    @Autowired
+    private EmailService service;
+
     @PostMapping
     public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+        service.sendEmail(dto);
         return ResponseEntity.noContent().build();
     }
 
---
+++ src/main/java/com/devsuperior/integrations/services/EmailService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.integrations.services;
+
+import com.devsuperior.integrations.dto.EmailDTO;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.*;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class EmailService {
+
+    private static Logger LOG = LoggerFactory.getLogger(EmailService.class);
+
+    @Autowired
+    private SendGrid sendGrid;
+
+    public void sendEmail(EmailDTO emailDTO) {
+        Email from = new Email(emailDTO.getFromEmail(), emailDTO.getFromName());
+        Email to = new Email(emailDTO.getTo());
+        Content content = new Content(emailDTO.getContentType(), emailDTO.getBody());
+        Mail mail = new Mail(from, emailDTO.getSubject(), to, content);
+
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            LOG.info("Sending email to: " + emailDTO.getTo());
+            Response response = sendGrid.api(request);
+            if (response.getStatusCode() >= 400 && response.getStatusCode() <= 500) {
+                LOG.error("Error sending email: " + response.getBody());
+            }
+            else {
+                LOG.info("Email sent! status = " + response.getStatusCode());
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
